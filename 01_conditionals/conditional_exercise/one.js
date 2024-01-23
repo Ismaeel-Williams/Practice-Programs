f13f881b4935248d4043c19d4c526951788ec509
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-01-23 15:26:34 +0200

    one.js snippet 1-0

---
+++ 01_conditionals/conditional_exercise/one.js
@@ -0,0 +1,15 @@
+//snippet 1-0
+//false
+//true
+//prints "swish" "pos"
+let qty = 38;
+
+if (qty > 30 && qty % 5 === 4) {
+    console.log("swish");
+} else {
+    console.log("swoosh");
+} 
+
+if (qty > 0) {
+    console.log("pos")
+}
