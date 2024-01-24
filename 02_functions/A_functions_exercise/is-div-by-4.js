17fd75f476963f31e3a2caab3b867ce6cae1da6f
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-01-24 13:56:47 +0200

    divby4&keepitquiet

---
+++ 02_functions/A_functions_exercise/is-div-by-4.js
@@ -0,0 +1,8 @@
+function isDivBy4(n) {
+if (n % 4 === 0) {
+console.log(true);;
+} else {
+    console.log(false);
+}};
+
+isDivBy4(802);
---
+++ 02_functions/A_functions_exercise/keep-it-quiet.js
@@ -0,0 +1,5 @@
+function keepItQuiet(str) {
+    console.log(str.toLowerCase() + " . . .")
+}
+
+keepItQuiet("THIS IS SPARTA")
