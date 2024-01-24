9db36183b29f37d8008abad798935592d8d84b3b
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-01-24 11:42:52 +0200

    one.js

--- 01_conditionals/conditional_exercise/one.js
+++ 01_conditionals/conditional_exercise/one.js
@@ -13,3 +13,43 @@ if (qty > 30 && qty % 5 === 4) {
 if (qty > 0) {
     console.log("pos")
 }
+
+// snippet 1-1
+//false
+//true
+//prints "beta"
+let a = "celery";
+let b = "SQUASH";
+
+if (a === a.toUpperCase()) {
+  console.log("alpha");
+}
+
+if (b === b.toUpperCase()) {
+  console.log("beta");
+}
+
+// snippet 1-2
+//true
+//prints "ding"
+let number = 9;
+
+if (number > 4) {
+  console.log("ding");
+} else if (number % 3 === 0) {
+  console.log("dong");
+}
+
+// snippet 1-3
+//true
+//true
+//prints "vroom" "skrrt"
+let z = 12;
+
+if (z > 10) {
+  console.log("vroom");
+}
+
+if (z % 3 === 0) {
+  console.log("skrrt");
+}
