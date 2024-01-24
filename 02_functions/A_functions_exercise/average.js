c5c59601389db9f7589afdcd72e539773f2cf45e
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-01-24 14:32:50 +0200

    average&endwithT

---
+++ 02_functions/A_functions_exercise/average.js
@@ -0,0 +1,5 @@
+function average(n1, n2, n3){
+    console.log((n1+n2+n3) / 3)
+}
+
+average(1, 2, 3)
---
+++ 02_functions/A_functions_exercise/endsWithT.js
@@ -0,0 +1,10 @@
+function endsWithT(str){
+    // let lowercase = str.toLowerCase()
+    if (str.toLowerCase()[str.length -1] === "t"){
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+};
+
+endsWithT("elevenT");
