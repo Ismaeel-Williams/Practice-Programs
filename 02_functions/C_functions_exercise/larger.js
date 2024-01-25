9e9e5f20346b793db8ce82ed9ea16b56c8c0b4ea
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-01-25 11:52:27 +0200

    larger&numberchange

---
+++ 02_functions/C_functions_exercise/larger.js
@@ -0,0 +1,13 @@
+function larger(n1, n2){
+    if (n1 >= n2){
+        console.log(n1)
+    } else {
+        console.log(n2)
+    }
+}
+
+larger(256, 400); // 400
+larger(31, 4); // 31
+larger(-6, 7); // 7
+larger(11.3, 11.2); // 11.3
+larger(-10, -3); // -3
---
+++ 02_functions/C_functions_exercise/number-change.js
@@ -0,0 +1,12 @@
+function numberChange(n){
+    if (n%2 === 0) {
+        console.log(n / 2)
+    } else {
+        console.log(n * 2)
+    }
+}
+
+numberChange(6); // 3
+numberChange(7); // 14
+numberChange(16); // 8
+numberChange(21); // 42
