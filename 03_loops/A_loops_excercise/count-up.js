f126808023c53d19ab2b9f323a622cbe71e80a4d
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-01-29 13:25:51 +0200

    countup-onetofour

---
+++ 03_loops/A_loops_excercise/count-up.js
@@ -0,0 +1,7 @@
+function countUp(max) {
+    for(i = 1; i <= max; i++){
+        console.log(i);
+    };
+}
+
+countUp(5);
---
+++ 03_loops/A_loops_excercise/one-to-four.js
@@ -0,0 +1,7 @@
+function onetofour() {
+    for ( let i = 1; i <=4; i++) {
+        console.log(i);
+    }
+}
+
+onetofour()
