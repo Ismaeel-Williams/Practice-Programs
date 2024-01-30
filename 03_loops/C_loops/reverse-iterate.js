c96bba870ff5c52dc438791eada67dae5c402513
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-01-30 13:34:31 +0200

    divisiblerange&reverseiterate

---
+++ 03_loops/C_loops/reverse-iterate.js
@@ -0,0 +1,20 @@
+function reverseIterate(str){
+    for (let i = str.length - 1; i >= 0; i--){
+        console.log(str[i]);
+    }
+}
+
+reverseIterate("carrot");
+// prints
+//  t
+//  o
+//  r
+//  r
+//  a
+//  c
+
+reverseIterate("box");
+// prints
+//  x
+//  o
+//  b
