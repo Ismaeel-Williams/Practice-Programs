9d6f89c9d28bcd7be535dca30d3f58f610596eb3
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-02-26 14:15:08 +0200

    getAverage

---
+++ 06_objects/B_objects_exercise/get-average-age.js
@@ -0,0 +1,23 @@
+function getAverageAge(arr){
+    let sumAge = 0
+    for(let i = 0; i < arr.length; i++){
+        sumAge += arr[i].age
+    }
+    let averageAge = sumAge / arr.length
+    return averageAge.toFixed(2)  
+}
+
+let peeps = [
+    { name: "Lovelace", age: 36, born: "London, UK" },
+    { name: "Kleene", age: 85, born: "Connecticut, US" },
+    { name: "Turing", age: 41, born: "London, UK" },
+    { name: "Hopper", age: 85, born: "New York, US" },
+  ];
+  console.log(getAverageAge(peeps)); // 61.75
+  
+  let people = [
+    { name: "Orwell", age: 46, born: "Bihar, India" },
+    { name: "Bradbury", age: 91, born: "California, US" },
+    { name: "Huxley", age: 69, born: "California, US" },
+  ];
+  console.log(getAverageAge(people)); // 86.66
